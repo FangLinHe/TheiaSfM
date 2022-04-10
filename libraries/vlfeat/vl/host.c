0b487c4937ccb8b1f1b900c320028ac5c4e2bbaf
Fang-Lin He <fanglin.he.ms@gmail.com>
2022-04-10 13:33:23 +0200

    Fix build issues on MacOS arm64
    Change-Id: Icc4353440a017b95d52fe8c7b2eacdd4d4a5baa5

--- libraries/vlfeat/vl/host.c
+++ libraries/vlfeat/vl/host.c
@@ -441,6 +441,7 @@ _vl_cpuid (vl_int32* info, int function)
 
 #endif
 
+#ifdef VL_ARCH_IX86
 void
 _vl_x86cpu_info_init (VlX86CpuInfo *self)
 {
@@ -487,6 +488,7 @@ _vl_x86cpu_info_to_string_copy (VlX86CpuInfo const *self)
   }
   return string ;
 }
+#endif
 
 /** ------------------------------------------------------------------
  ** @brief Human readable static library configuration
